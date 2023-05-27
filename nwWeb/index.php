76100f7e042e0c8ead6d5035e6a11a718f619db2
BlindSqrl <joseph.a.hiatt@gmail.com>
2023-05-27 12:32:17 -0700

    still problems

--- nwWeb/index.php
+++ nwWeb/index.php
@@ -6,7 +6,7 @@
             <meta http-equiv="X-UA-Compatible" content="IE=edge">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-            <link rel="stylesheet" type="text/css" href="/css/style.css">
+            <link rel="stylesheet" type="text/css" href="/nwWeb/css/style.css">
     </head>
     <body>
         <!-- Bootstrap Navbar -->
